f6effb0768c87d5d3f6eee4344bf6cb2691764d1
snamiki1212 <snamiki1212@gmail.com>
2021-07-14 17:16:50 -0700

    Add colorize feature

--- objc-input/ViewController.m
+++ objc-input/ViewController.m
@@ -8,17 +8,51 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic, strong) UITextField *textField;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-    self.view.backgroundColor = UIColor.redColor;
+    self.view.backgroundColor = UIColor.whiteColor;
+    
+    // text field
+    _textField = [[UITextField alloc] initWithFrame:CGRectMake(0, 0, 100, 200)];
+    [self.view addSubview:_textField];
+    [_textField setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [_textField.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor].active = YES;
+    [_textField.centerYAnchor constraintEqualToAnchor:self.view.centerYAnchor].active = YES;
+    [_textField.widthAnchor constraintEqualToAnchor:self.view.widthAnchor multiplier:0.9].active = YES;
+    _textField.backgroundColor = UIColor.lightGrayColor; // TODO:
+    [_textField addTarget:self action:@selector(onChange:) forControlEvents:UIControlEventEditingChanged];
     
 }
 
+- (void) onChange:(UITextField *) sender
+{
+    NSMutableAttributedString *text = [[NSMutableAttributedString alloc] initWithString:sender.text];
+    NSString *string = [[NSString alloc] initWithString:sender.text];
+    NSString *substring = @"red";
+    
+    NSRange searchRange = NSMakeRange(0, string.length);
+    NSRange foundRange;
+    while (searchRange.location < string.length) {
+        searchRange.length = string.length - searchRange.location;
+        foundRange = [string rangeOfString:substring options:NSCaseInsensitiveSearch range:searchRange];
+        if (foundRange.location != NSNotFound) {
+            // found an occurrence of the substring! do stuff here
+            [text addAttribute:NSForegroundColorAttributeName value:[UIColor colorWithRed:1 green:0 blue:0 alpha:1] range:foundRange];
+            
+            // next step
+            searchRange.location = foundRange.location+foundRange.length;
+        } else {
+            // no more substring to find
+            break;
+        }
+    }
+    
+    _textField.attributedText = text;
+}
 
 @end
